e4ae13fbba3706670691927c36d3029fc94bfcd6
Marcus Vinicius Araujo <mvsabino@gmail.com>
2021-08-23 00:42:17 -0300

    ADD comment_spec.rb

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  it { should belong_to(:article) }
+
+  it { should validate_presence_of(:commenter) }
+  it { should validate_presence_of(:body) }
+end
